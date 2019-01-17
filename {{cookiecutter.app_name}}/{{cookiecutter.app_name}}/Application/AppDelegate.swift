dd06422b9e3bdfea500519f985e6d02ba87fc093
Cyan <R.swift>
2019-01-17 11:43:41 +0800

    modifie swiftlint

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/Application/AppDelegate.swift
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/Application/AppDelegate.swift
@@ -10,13 +10,13 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    lazy private var router = RootRouter()
     lazy private var deeplinkHandler = DeeplinkHandler()
     lazy private var notificationsHandler = NotificationsHandler()
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
         // Notifications
         notificationsHandler.configure()
 
